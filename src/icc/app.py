330890546547f0b9c53eaf933f9935e6a1779cc7
Evgeny Cherkashin <eugene@irnok.net>
2013-04-20 13:06:51 +0900

    Added modularized static content under icc.www namespace module peixe.

--- src/icc/app.py
+++ src/icc/app.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     config = Configurator()
     config.add_route('hello', '/hello/{name}')
     config.add_view(hello_world, route_name='hello')
+    config.add_static_view(name='static', path='icc.www.peixe:/www/static')
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
