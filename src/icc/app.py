c55e2a478130359f2459605b0acdb115bb86e409
Evgeny Cherkashin <eugene@irnok.net>
2013-04-21 01:19:56 +0900

    Setup and cache UNIVERSE graph.

--- src/icc/app.py
+++ src/icc/app.py
@@ -4,9 +4,24 @@ from pyramid.response import Response
 
 import rdflib
 
-g=UNIVERSE=rdflib.Graph()
-g.parse("http://www.w3.org/People/Berners-Lee/card")
-print("graph has %s statements." % len(g))
+UNIVERSE=rdflib.Graph()
+CACHE_NAME='card.n3'
+
+def universe_setup(g):
+
+    try:
+        cf=open(CACHE_NAME)
+        g.parse(cf, format='n3')
+    except IOError:
+        g.parse("http://www.w3.org/People/Berners-Lee/card")
+        s = g.serialize(format='n3')
+        cf=open(CACHE_NAME, 'w')
+        cf.write(s)
+        cf.close()
+
+    print("graph has %s statements." % len(g))
+
+universe_setup(UNIVERSE)
 
 
 def view_subject(request):
