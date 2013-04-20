b67446ef1bb7cbcd2b8253b37ca1a7968617475a
Evgeny Cherkashin <eugene@irnok.net>
2013-04-20 13:07:28 +0900

    Added main function as root index page.

--- src/icc/app.py
+++ src/icc/app.py
@@ -2,15 +2,24 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 
-
 def hello_world(request):
-    return Response('Hello %(name)s!' % request.matchdict)
+    return Response('Hello %(name)s! \%(text)s\.' % request.matchdict)
+
+def view_main(request):
+    return Response('fred')
 
 if __name__ == '__main__':
     config = Configurator()
-    config.add_route('hello', '/hello/{name}')
+    config.add_route('hello', '/hello/{name}/{text}')
+    config.add_route('index', '/')
     config.add_view(hello_world, route_name='hello')
+    config.add_view(view_main, route_name='index')
     config.add_static_view(name='static', path='icc.www.peixe:/www/static')
     app = config.make_wsgi_app()
+    print "Starting server."
     server = make_server('0.0.0.0', 8080, app)
-    server.serve_forever()
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        server.server_close()
+        print "KeyboardInterrupt, closed."
