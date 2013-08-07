e8e3863cd5aebe7d465859e5b2b8cb096de8c7f4
Evgeny Cherkashin <eugeneai@irnok.net>
2013-08-07 16:31:06 +0900

    Added installation of kyotocabinet python library.

--- script/installer.sh
+++ script/installer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-pip install https://github.com/RDFLib/rdflib/tarball/master/
-pip install https://github.com/eugeneai/rdflib-kyotocabinet/tarball/master/
+#pip install https://github.com/RDFLib/rdflib/tarball/master/
+pip install http://fallabs.com/kyotocabinet/pythonlegacypkg/kyotocabinet-python-legacy-1.18.tar.gz
+#pip install https://github.com/eugeneai/rdflib-kyotocabinet/tarball/master/
