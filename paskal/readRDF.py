509b6054583f6ab1f0dd8ba2e4eb0d5ebdfb4546
Evgeny Cherkashin <eugene@irnok.net>
2014-01-29 23:07:46 +0900

    Removed redundant operations.

--- paskal/readRDF.py
+++ paskal/readRDF.py
@@ -1,17 +1,20 @@
 import rdflib
 from rdflib.graph import ConjunctiveGraph as Graph
-from rdflib import plugin
-from rdflib.store import Store
 
-configString = "/tmp/rdfstore"
-
-store = plugin.get('KyotoCabinet', Store)('rdfstore')
+configString = "/tmp/rdfstore12"
 
 graph = Graph(store="KyotoCabinet")
 path = configString
 rt = graph.open(path, create=False)
 
 print "Triples in graph: ", len(graph)
-print graph.serialize()
+print "first 20 are as follows:"
+i=20
+for subj, pred, obj in graph:
+    print(subj, pred, obj)
+    i-=1
+    if i==0:
+	break
+
 
 graph.close()
--- paskal/writeRDF.py
+++ paskal/writeRDF.py
@@ -1,11 +1,7 @@
 import rdflib
 from rdflib.graph import ConjunctiveGraph as Graph
-from rdflib import plugin
-from rdflib.store import Store
 
-configString = "/tmp/rdfstore"
-
-store = plugin.get('KyotoCabinet', Store)('rdfstore')
+configString = "/tmp/rdfstore12"
 
 graph = Graph(store="KyotoCabinet")
 path = configString
@@ -13,10 +9,10 @@ rt = graph.open(path, create=True)
 
 print "Triples in graph before add: ", len(graph)
 graph.parse("http://130.88.198.11/co-ode-files/ontologies/pizza.owl", format="xml")
-for subj, pred, obj in graph:
-    if (subj, pred, obj) not in graph:
-        raise Exception("It better be!")
-    graph.add((subj, pred, obj))
+#for subj, pred, obj in graph:
+#    if (subj, pred, obj) not in graph:
+#        raise Exception("It better be!")
+#    graph.add((subj, pred, obj))
 graph.commit()
 
 print "Triples in graph after add: ", len(graph)
